5fca38f8e8ebbb342f23fcc5ba8302d174c83a0d
Matheus H. de Souza <mh.matheussouza@gmail.com>
2020-02-26 20:43:12 -0300

    Refatorando para utilizar variáveis de ambiente na aplicação

--- email-workers-compose/app/sender.py
+++ email-workers-compose/app/sender.py
@@ -1,15 +1,24 @@
 import psycopg2
 import redis
 import json
+import os
 from bottle import Bottle, request
 
 class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
-        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
-        DSN = 'dbname=email_sender user=postgres password=postgres host=db'
-        self.conn = psycopg2.connect(DSN)
+
+        redis_host = os.getenv('REDIS_HOST', 'queue')
+        redis_port = os.getenv('REDIS_PORT', 6379)
+        self.fila = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
+
+        db_host = os.getenv('DB_HOST', 'db')
+        db_user = os.getenv('DB_USER', 'postgres')
+        db_password = os.getenv('DB_PASSWORD', 'postgres')
+        db_name = os.getenv('DB_NAME', 'sender')
+        dsn = f'dbname={db_name} user={db_user} password={db_password} host={db_host}'
+        self.conn = psycopg2.connect(dsn)
 
     def register_message(self, assunto, mensagem):
         SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
